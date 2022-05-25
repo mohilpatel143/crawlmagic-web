1948a90acc7044cfbb62eb25a7979be8990f7f19
mohil <patelmohil0607@gmail.com>
2022-05-25 19:21:10 +0530

    create routers in navbar menus

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,5 +10,95 @@ urlpatterns = [
     path("contact-us/", views.contactus, name="contact-us"),
     path("career/", views.career, name="career"),
     path("support/", views.support, name="support"),
-    path("404/", views.notfound, name="404")
+    path("404/", views.notfound, name="404"),
+
+
+    # ======================= Services / Web_Scraping_Services =======================
+    path("enterprise_web_crawling/", views.enterprise_web_crawling, name="enterprise_web_crawling"),
+    path("mobile_app_scraping/", views.mobile_app_scraping, name="mobile_app_scraping"),
+    path("web_scraping_API/", views.web_scraping_API, name="web_scraping_API"),
+    path("hosted_web_crawling_services/", views.hosted_web_crawling_services, name="hosted_web_crawling_services"),
+
+    # ======================= Services / Advanced Scraping Services =======================
+    path("python_scrapy_consulting/", views.python_scrapy_consulting, name="python_scrapy_consulting"),
+    path("robotic_process_automation/", views.robotic_process_automation, name="robotic_process_automation"),
+    path("dark_and_deep_web_data_scraping/", views.dark_and_deep_web_data_scraping, name="dark_and_deep_web_data_scraping"),
+
+
+    # ======================= Services / Data Enrichment =======================
+    path("data_appending/",views.data_appending, name="data_appending"),
+    path("image_OCR_services/", views.image_OCR_services, name="image_OCR_services"),
+    path("data_entry_and_data_processing/", views.data_entry_and_data_processing, name="data_entry_and_data_processing"),
+
+
+    # ======================= Services / AI & Data Analytics =======================
+    path("machine_learning/", views.machine_learning, name="machine_learning"),
+    path("sentiment_analysis/", views.sentiment_analysis, name="sentiment_analysis"),
+    path("data_visualization/", views.data_visualization, name="data_visualization"),
+
+
+    # ======================= Industries/E-commerce ======================= 
+    path("retail_pricing_intelligence/", views.retail_pricing_intelligence, name="retail_pricing_intelligence"),
+    path("price_monitoring/", views.price_monitoring, name="price_monitoring"),
+    path("pricing_intelligence/", views.pricing_intelligence, name="pricing_intelligence"),
+    path("channel_pricing_intelligence/", views.channel_pricing_intelligence, name="channel_pricing_intelligence"),
+    path("tyre_pricing_intelligence/", views.tyre_pricing_intelligence, name="tyre_pricing_intelligence"),
+    path("brand_monitoring/", views.brand_monitoring, name="brand_monitoring"),
+    path("product_pricing_and_review_data_scraping/", views.product_pricing_and_review_data_scraping, name="product_pricing_and_review_data_scraping"),
+    path("amazon_data_scraping/", views.amazon_data_scraping, name="amazon_data_scraping"),
+    path("ecommerce_pricing_intelligence/", views.ecommerce_pricing_intelligence, name="ecommerce_pricing_intelligence"),
+    path("lazada_data_scraping_services/", views.lazada_data_scraping_services, name="lazada_data_scraping_services"),
+    path("shopee_data_scraping_service/", views.shopee_data_scraping_service, name="shopee_data_scraping_service"),
+
+    # ======================= Travel ======================= 
+    path("flight_pricing_intelligence/", views.flight_pricing_intelligence, name="flight_pricing_intelligence"),
+    path("travel_data_intelligence/", views.travel_data_intelligence, name="travel_data_intelligence"),
+    path("vacation_rental_intelligence/", views.vacation_rental_intelligence, name="vacation_rental_intelligence"),
+    path("hotel_pricing_intelligence/", views.hotel_pricing_intelligence, name="hotel_pricing_intelligence"),
+
+    # ======================= Food Delivery ======================= 
+    path("competitor_pricing_for_food_delivery/", views.competitor_pricing_for_food_delivery, name="competitor_pricing_for_food_delivery"),
+    path("food_delivery_aggregator/", views.food_delivery_aggregator, name="food_delivery_aggregator"),
+    path("grocery_delivery_app_scraping_services/", views.grocery_delivery_app_scraping_services, name="grocery_delivery_app_scraping_services"),
+    path("online_food_ordering_app_scraping/", views.online_food_ordering_app_scraping, name="online_food_ordering_app_scraping"),
+    path("nutrition_facts_food_data_scraping_services/", views.nutrition_facts_food_data_scraping_services, name="nutrition_facts_food_data_scraping_services"),
+    path("restaurant_data_scraping/", views.restaurant_data_scraping, name="restaurant_data_scraping"),
+    path("uber_eats_data_scraping_services/", views.uber_eats_data_scraping_services, name="uber_eats_data_scraping_services"),
+    path("zomato_restaurant_data_scraping/", views.zomato_restaurant_data_scraping, name="zomato_restaurant_data_scraping"),
+
+    # ======================= Social Media ======================= 
+    path("facebook_data_scraping/", views.facebook_data_scraping, name="facebook_data_scraping"),
+    path("instagram_data_scraping/", views.instagram_data_scraping, name="instagram_data_scraping"),
+    path("linkedIn_data_scraping/", views.linkedIn_data_scraping, name="linkedIn_data_scraping"),
+    path("quora_data_scraping/", views.quora_data_scraping, name="quora_data_scraping"),
+    path("tikTok_data_scraping/", views.tikTok_data_scraping, name="tikTok_data_scraping"),
+    path("twitter_data_scraping/", views.twitter_data_scraping, name="twitter_data_scraping"),
+    path("youTube_data_scraping/", views.youTube_data_scraping, name="youTube_data_scraping"),
+    path("social_media_monitoring/", views.social_media_monitoring, name="social_media_monitoring"),
+
+    # ======================= Entertainment ======================= 
+    path("streaming_OTT_platform_app_data/", views.streaming_OTT_platform_app_data, name="streaming_OTT_platform_app_data"),
+    path("movie_showtime_intelligence/", views.movie_showtime_intelligence, name="movie_showtime_intelligence"),
+    path("netflix_data_scraping/", views.netflix_data_scraping, name="netflix_data_scraping"),
+
+    # ======================= Real Estate ======================= 
+    path("real_estate_data_intelligence/", views.real_estate_data_intelligence, name="real_estate_data_intelligence"),
+
+    # ========================== Recruitment ==========================  
+    path("job_listings_and_data_feeds/", views.job_listings_and_data_feeds, name="job_listings_and_data_feeds"),
+    path("human_capital_management/", views.human_capital_management, name="human_capital_management"),
+
+    # ========================== News & Media ==========================
+    path("news_aggregation_and_intelligence/", views.news_aggregation_and_intelligence, name="news_aggregation_and_intelligence"),
+    path("data_for_research_and_journalism_scraping/", views.data_for_research_and_journalism_scraping, name="data_for_research_and_journalism_scraping"),
+
+    # ========================== Finance ==========================  
+    path("stock_market_and_financial_data_scraping/", views.stock_market_and_financial_data_scraping, name="stock_market_and_financial_data_scraping"),
+    path("alternative_data/", views.alternative_data, name="alternative_data"),
+
+    # ========================== Data Aggregation ==========================  
+    path("data_intelligence/", views.data_intelligence, name="data_intelligence"),
+    path("location_intelligence/", views.location_intelligence, name="location_intelligence"),
+    path("customer_intelligence/", views.customer_intelligence, name="customer_intelligence"),
+    path("sales_intelligence/", views.sales_intelligence, name="sales_intelligence"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -3,6 +3,9 @@ from .models import Subscrib, Contact
 from django.core.mail import send_mail
 
 # Create your views here.
+
+
+# ========================== Main Header ==========================
 def home(request):
     if request.method=="POST":
         email = request.POST.get('email', '')
@@ -54,3 +57,210 @@ def support(request):
 
 def notfound(request):
     return render(request, 'webapp/404.html')
+
+
+# ========================== Services / Web_Scraping_Services ==========================
+def enterprise_web_crawling(request):
+    return render(request, 'webapp/services/web_scraping_services/enterprise_web_crawling.html')
+
+def mobile_app_scraping(request):
+    return render(request, 'webapp/services/web_scraping_services/mobile_app_scraping.html')
+
+def web_scraping_API(request):
+    return render(request, 'webapp/services/web_scraping_services/web_scraping_API.html')    
+
+def hosted_web_crawling_services(request):
+    return render(request, 'webapp/services/web_scraping_services/hosted_web_crawling_services.html')    
+
+
+# ========================== Services / Advanced Scraping Services ==========================
+def python_scrapy_consulting(request):
+    return render(request, 'webapp/services/advanced_scraping_services/python_scrapy_consulting.html')
+
+def robotic_process_automation(request):
+    return render(request, 'webapp/services/advanced_scraping_services/robotic_process_automation.html')    
+
+def dark_and_deep_web_data_scraping(request):
+    return render(request, 'webapp/services/advanced_scraping_services/dark_and_deep_web_data_scraping.html')    
+
+
+# ========================== Services / Data Enrichment ==========================
+def data_appending(request):
+    return render(request, 'webapp/services/data_enrichment/data_appending.html')
+
+def image_OCR_services(request):
+    return render(request, 'webapp/services/data_enrichment/image_OCR_services.html')  
+
+def data_entry_and_data_processing(request):
+    return render(request, 'webapp/services/data_enrichment/data_entry_and_data_processing.html')      
+
+
+
+# ========================== Services / AI & Data Analytics ==========================
+def machine_learning(request):
+    return render(request, 'webapp/services/AI_and_data_analytics/machine_learning.html')
+
+def sentiment_analysis(request):
+    return render(request, 'webapp/services/AI_and_data_analytics/sentiment_analysis.html')    
+
+def data_visualization(request):
+    return render(request, 'webapp/services/AI_and_data_analytics/data_visualization.html')    
+
+
+
+
+
+# ========================== Idustries / E-commerce ==========================
+def retail_pricing_intelligence(request):
+    return render(request, 'webapp/industries/e-commerce/retail_pricing_intelligence.html')
+
+def price_monitoring(request):
+    return render(request, 'webapp/industries/e-commerce/price_monitoring.html')    
+
+def pricing_intelligence(request):
+    return render(request, 'webapp/industries/e-commerce/pricing_intelligence.html')    
+
+def channel_pricing_intelligence(request):
+    return render(request, 'webapp/industries/e-commerce/channel_pricing_intelligence.html')    
+
+def tyre_pricing_intelligence(request):
+    return render(request, 'webapp/industries/e-commerce/tyre_pricing_intelligence.html')
+
+def brand_monitoring(request):
+    return render(request, 'webapp/industries/e-commerce/brand_monitoring.html')    
+
+def product_pricing_and_review_data_scraping(request):
+    return render(request, 'webapp/industries/e-commerce/product_pricing_and_review_data_scraping.html')    
+
+def amazon_data_scraping(request):
+    return render(request, 'webapp/industries/e-commerce/amazon_data_scraping.html')   
+
+def ecommerce_pricing_intelligence(request):
+    return render(request, 'webapp/industries/e-commerce/ecommerce_pricing_intelligence.html')     
+
+def lazada_data_scraping_services(request):
+    return render(request, 'webapp/industries/e-commerce/lazada_data_scraping_services.html')
+
+def shopee_data_scraping_service(request):
+    return render(request, 'webapp/industries/e-commerce/shopee_data_scraping_service.html')    
+
+
+
+# ========================== Idustries / Travel ========================== 
+def flight_pricing_intelligence(request):
+    return render(request, 'webapp/industries/travel/flight_pricing_intelligence.html')
+
+def travel_data_intelligence(request):
+    return render(request, 'webapp/industries/travel/travel_data_intelligence.html')  
+
+def vacation_rental_intelligence(request):
+    return render(request, 'webapp/industries/travel/vacation_rental_intelligence.html')      
+
+def hotel_pricing_intelligence(request):
+    return render(request, 'webapp/industries/travel/hotel_pricing_intelligence.html')    
+
+
+# ========================== Idustries / Food Delivery ========================== 
+def competitor_pricing_for_food_delivery(request):
+    return render(request, 'webapp/industries/food_delivery/competitor_pricing_for_food_delivery.html')
+
+def food_delivery_aggregator(request):
+    return render(request, 'webapp/industries/food_delivery/food_delivery_aggregator.html')   
+
+def grocery_delivery_app_scraping_services(request):
+    return render(request, 'webapp/industries/food_delivery/grocery_delivery_app_scraping_services.html')     
+
+def online_food_ordering_app_scraping(request):
+    return render(request, 'webapp/industries/food_delivery/online_food_ordering_app_scraping.html')    
+
+def nutrition_facts_food_data_scraping_services(request):
+    return render(request, 'webapp/industries/food_delivery/nutrition_facts_food_data_scraping_services.html')    
+
+def restaurant_data_scraping(request):
+    return render(request, 'webapp/industries/food_delivery/restaurant_data_scraping.html')   
+
+def uber_eats_data_scraping_services(request):
+    return render(request, 'webapp/industries/food_delivery/uber_eats_data_scraping_services.html')     
+
+def zomato_restaurant_data_scraping(request):
+    return render(request, 'webapp/industries/food_delivery/zomato_restaurant_data_scraping.html')    
+
+
+# ========================== Idustries / Social Media ==========================     
+def facebook_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/facebook_data_scraping.html')
+
+def instagram_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/instagram_data_scraping.html')    
+
+def linkedIn_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/linkedIn_data_scraping.html')    
+
+def quora_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/quora_data_scraping.html')    
+
+def tikTok_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/tikTok_data_scraping.html')    
+
+def twitter_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/twitter_data_scraping.html') 
+
+def youTube_data_scraping(request):
+    return render(request, 'webapp/industries/social_media/youTube_data_scraping.html')      
+
+def social_media_monitoring(request):
+    return render(request, 'webapp/industries/social_media/social_media_monitoring.html')     
+
+
+# ========================== Idustries / Entertainment ==========================  
+
+def streaming_OTT_platform_app_data(request):
+    return render(request, 'webapp/industries/entertainment/streaming_OTT_platform_app_data.html')
+
+def movie_showtime_intelligence(request):
+    return render(request, 'webapp/industries/entertainment/movie_showtime_intelligence.html')
+
+def netflix_data_scraping(request):
+    return render(request, 'webapp/industries/entertainment/netflix_data_scraping.html')    
+
+
+# ========================== Idustries / Real Estate ==========================     
+def real_estate_data_intelligence(request):
+    return render(request, 'webapp/industries/real_estate/real_estate_data_intelligence.html')
+
+
+# ========================== Idustries / Recruitment ========================== 
+def job_listings_and_data_feeds(request):
+    return render(request, 'webapp/industries/recruitment/job_listings_and_data_feeds.html')
+
+def human_capital_management(request):
+    return render(request, 'webapp/industries/recruitment/human_capital_management.html')
+
+
+# ========================== Idustries / News & Media ==========================    
+def news_aggregation_and_intelligence(request):
+    return render(request, 'webapp/industries/news_and_media/news_aggregation_and_intelligence.html')
+
+def data_for_research_and_journalism_scraping(request):
+    return render(request, 'webapp/industries/news_and_media/data_for_research_and_journalism_scraping.html')    
+
+
+# ========================== Idustries / Finance ==========================  
+def stock_market_and_financial_data_scraping(request):
+    return render(request, 'webapp/industries/finance/stock_market_and_financial_data_scraping.html')  
+
+def alternative_data(request):
+    return render(request, 'webapp/industries/finance/alternative_data.html')    
+
+# ========================== Idustries / Data Aggregation ==========================    
+def data_intelligence(request):
+    return render(request, 'webapp/industries/data_aggregation/data_intelligence.html')
+
+def location_intelligence(request):
+    return render(request, 'webapp/industries/data_aggregation/location_intelligence.html')    
+
+def customer_intelligence(request):
+    return render(request, 'webapp/industries/data_aggregation/customer_intelligence.html')
+
+def sales_intelligence(request):
+    return render(request, 'webapp/industries/data_aggregation/sales_intelligence.html')        
